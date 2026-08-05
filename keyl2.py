881df63d81635c3ee37383efa3eaeaf93d6c5e68
Collins Karweru <Collinskarweru@gmail.com>
2026-08-05 14:47:12 +0300

    commit

---
+++ keyl2.py
@@ -0,0 +1,32 @@
+from pynput import keyboard
+import sys
+
+def on_press(key):
+    try:
+        sys.stdout.write(key.char)
+        sys.stdout.flush()
+
+    except AttributeError:
+        if key == keyboard.Key.enter:
+            sys.stdout.write("\n")
+        elif key == keyboard.Key.tab:
+            sys.stdout.write("\t")
+        elif key == keyboard.Key.space:
+            sys.stdout.write(" ")
+        elif key == keyboard.Key.backspace:
+            sys.stdout.write("\b \b")
+    
+    sys.stdout.flush()
+
+def on_release (key):
+    if key == keyboard.Key.esc:
+        print("Exiting")
+        return False
+
+print("Keyboard Logger Activated")
+print("Press Esc to get out\n")
+
+with keyboard.Listener (on_press=on_press, on_release=on_release) as listener: 
+    listener.join()
+
+
